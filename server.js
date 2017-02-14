ba7fa4fdd4a6031df87b6567effc0aad73934113
keyserbrian1 <keyserbrian1@github.com>
2017-02-14 10:18:15 -0600

    Added my server.js and my static file loader

--- server.js
+++ server.js
@@ -1,20 +1,67 @@
-var mongoose = require( 'mongoose' ),
-    express  = require( 'express' ),
-    bp       = require( 'body-parser' ),
-    path     = require( 'path' ),
-    database = require('./server/config/mongoose.js'),
-    routes_setter = require('./server/config/routes.js'),
-    root     = __dirname,
-    port     = process.env.PORT || 8000,
-    app      = express();
+var express = require("express");
+var app = express();
 
-app.use( express.static( path.join( root, 'client' )));
-app.use( express.static( path.join( root, 'server' )));
-app.use( express.static( path.join( root, 'node_modules' )));
-app.use(bp.json());
+var q = require("q");
 
-routes_setter(app);
+var socketIO = require("socket.io");
 
-app.listen( port, function() {
-  console.log( `server running on port ${ port }` );
+app.use(require("body-parser").json());
+
+var pgp = require("pg-promise")();
+var db = pgp({
+  "database": "chat",
+  "user": "coder65535",
+  "password": "Brian1",
+  "host": "localhost",
+  "port": "5432"
 });
+var session = require("express-session");
+var pgSession = require("connect-pg-simple")(session);
+app.use(session({
+  store: new pgSession({
+    pg:pgp.pg,
+    conString:{
+      "database": "chat",
+      "user": "coder65535",
+      "password": "Brian1",
+      "host": "localhost",
+      "port": "5432"
+    }
+  }),
+  saveUninitialized:true,
+  secret:"SecretPassForSessionData",
+  resave:"keep"
+}));
+
+var static_loader = require("utils");
+
+
+var ioDelayed = q.defer();
+
+var routes = require("./server/config/routes.js");
+routes(app, ioDelayed.promise, db);
+
+static_loader.install(app);
+
+app.set("views", __dirname + "/client");
+app.set("view engine", "ejs");
+
+// NOTE: Begin static page routing block
+app.get("/", function(req, res){
+  if (req.session.user){
+    res.redirect("/trading/");
+    return;
+  }
+  static_loader.serve_static(res, "index.html");
+});
+// NOTE: End static page routing block
+
+var server = app.listen(8000, function () {
+  console.log("Listening");
+});
+
+var io = socketIO.listen(server);
+// var ioSession = require("io-session");
+// io.use(ioSession(session));
+
+ioDelayed.resolve(io);
