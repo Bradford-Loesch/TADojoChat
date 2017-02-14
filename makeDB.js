f79a9ed6d95e16e0d91b179956529a543e855060
keyserbrian1 <keyserbrian1@github.com>
2017-02-14 12:28:30 -0600

    Fixed makeDB

--- makeDB.js
+++ makeDB.js
@@ -1,15 +1,24 @@
 var pgtools = require("pgtools");
 var pgp = require("pg-promise")();
-const config = {
-  "database": "chat",
+var config = {
   "user": "coder65535",
   "password": "Brian1",
   "host": "localhost",
   "port": "5432"
 };
-pgtools.createdb(config, "chat").then(()=>{
-  var db = pgp(config);
-  return db.any("CREATE TABLE 'session' (\n'sid' varchar NOT NULL COLLATE 'default',\n'sess' json NOT NULL,\n'expire' timestamp(6) NOT NULL\n)\nWITH (OIDS=FALSE);\nALTER TABLE 'session' ADD CONSTRAINT 'session_pkey' PRIMARY KEY ('sid') NOT DEFERRABLE INITIALLY IMMEDIATE;").then(()=>{
-    return db.any("CREATE TABLE 'user' ('username' varchar NOT NULL, 'password varchar NOT NULL')");
+pgtools.dropdb(config, "chat").then(()=>{
+  return pgtools.createdb(config, "chat").then(()=>{
+    config.database = "chat";
+    var db = pgp(config);
+    return db.any("CREATE TABLE session (sid varchar NOT NULL, sess json NOT NULL, expire timestamp(6) NOT NULL) WITH (OIDS=FALSE); ALTER TABLE session ADD CONSTRAINT session_pkey PRIMARY KEY (sid) NOT DEFERRABLE INITIALLY IMMEDIATE;").then(()=>{
+      return db.any("CREATE TABLE Users (id integer PRIMARY KEY NOT NULL, username varchar NOT NULL, password varchar NOT NULL);").then(()=>{
+        console.log("done");
+        process.exit(0);
+        return null;//make linter happy
+      }).catch((err)=>{
+        console.error(err);
+        console.error("in my create");
+      });
+    });
   });
 }).catch(console.error);
