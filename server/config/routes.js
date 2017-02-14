e49bb3caad10b7e29223fe0ee39872c3bfa12a30
keyserbrian1 <keyserbrian1@github.com>
2017-02-14 11:26:34 -0600

    Setup login/reg server side

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,8 @@
-var UsersCtrl = require('./../controllers/UsersCtrl.js');
+var UsersCtrl = require("./../controllers/UsersCtrl.js");
 
-module.exports = function(app) {
-    app.get('/users', UsersCtrl.index);
-    app.get('/users/:id', UsersCtrl.show);
-    app.post('/users', UsersCtrl.create);
-    app.put('/users/:id', UsersCtrl.update);
-    app.delete('/users/:id', UsersCtrl.delete);
-}
+module.exports = function(app, ioPromise, db) {
+  UsersCtrl.setDB(db);
+  app.get("/register", UsersCtrl.register);
+  app.get("/login", UsersCtrl.login);
+  app.get("/logout", UsersCtrl.logout);
+};
--- server/controllers/UsersCtrl.js
+++ server/controllers/UsersCtrl.js
@@ -1,62 +1,53 @@
-var mongoose = require('mongoose'),
-    SCHEMANAME = mongoose.model('SCHEMANAME');
+var bcrypt = require("bcrypt");
+var db = null;
 
-function SCHEMANAMEController() {
-    this.index = function(req, res) {
-        SCHEMANAME.find({}, function(err, response) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                res.json(response);
-            }
-        });
+module.exports = {
+  setDB:function(dbObj){
+    db = db||dbObj;
+  },
+  register: function(req, res){
+    console.log("start");
+    var data = req.body;
+    if (data.password !== data.passconf){
+      res.json({success:false, err:{header:"Error in register:",items:["Password does not match confirmation"]}});
+      return;
     }
-    this.show = function(req, res) {
-        SCHEMANAME.findOne({_id: req.params.id}, function(err, response) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                res.json(response);
-            }
-        });
-    }
-    this.create = function(req, res) {
-        var VARIABLENAMEInstance = new SCHEMANAME(req.body);
-        VARIABLENAMEInstance.created = new Date();
-        VARIABLENAMEInstance.save(function(err){
-            if(err){
-                console.log(err);
-            }
-            else {
-                console.log("Succesfully saved item");
-                res.json({Operation: 'Success'})
-            }
-        });
-    }
-    this.update = function(req, res) {
-        SCHEMANAME.update({_id: req.params.id}, req.body, function(err){
-            if(err){
-                console.log(err);
-            }
-            else {
-                console.log("Succesfully updated item");
-                res.json({Operation: 'Success'})
-            }
-        });
-    }
-    this.delete = function(req, res) {
-        SCHEMANAME.remove({_id: req.params.id}, function(err){
-            if(err){
-                console.log(err);
-            }
-            else {
-                console.log("Succesfully deleted item");
-                res.json({Operation: 'Success'})
-            }
-        })
-    }
-}
+    bcrypt.hash(data.password,15).then((pass)=>db.one("INSERT INTO users_user(email, username, password, created_at, updated_at) VALUES($1,$2,$3, NOW(), NOW()) RETURNING id",[data.email, data.username, pass]))
+    .then((user)=>{
+      console.log("new user ",user);
 
-module.exports = new SCHEMANAMEController();
+      req.session.user = user.id;
+      res.json({success:true});
+      return null;
+    }).catch((err)=>{
+      console.log("here");
+      res.json({success:false, err:{header:"Error in register:",items:[err.message||err]}});
+    });
+  },
+  login: function(req, res){
+    var data = req.body;
+    console.log("hi");
+    console.log(data);
+    db.oneOrNone("SELECT * FROM users_user WHERE username=$1",[data.username]).then(function(user){
+      if (!user){
+        throw "User not found";
+      }
+      return Promise.all([user, bcrypt.compare(req.body.password, user.password)]);
+    }).then(function(result){
+      var valid = result[1];
+      if (valid){
+        req.session.user = result[0].id;
+        res.json({success:true});
+      } else {
+        throw "Bad password";
+      }
+      return null;
+    }).catch(function(err){
+      res.json({success:false, err:{header:"Error in login.",items:[err.message||err]}});
+    });
+  },
+  logout:function(req, res){
+    delete req.session.user;
+    res.redirect("/");
+  }
+};
