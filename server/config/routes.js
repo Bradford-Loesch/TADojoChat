b223c641555e73d68fc18e653de95f3fedfedd92
keyserbrian1 <keyserbrian1@github.com>
2017-02-16 11:14:52 -0600

    set routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,20 @@
-var UsersCtrl = require("./../controllers/UsersCtrl.js");
+var UsersCtrl = require("../controllers/UsersCtrl.js");
+var ChatCtrl = require("../controllers/ChatCtrl.js");
 
 module.exports = function(app, ioPromise, db) {
   UsersCtrl.setDB(db);
   app.get("/register", UsersCtrl.register);
   app.get("/login", UsersCtrl.login);
   app.get("/logout", UsersCtrl.logout);
+  app.get("/profile", UsersCtrl.getMe);
+  app.get("/profile/:id", UsersCtrl.getUser);
+  app.patch("/profile", UsersCtrl.changeMe);
+  app.patch("/profile/:id", UsersCtrl.changeUser);
+  app.delete("/profile", UsersCtrl.deleteMe);
+  app.delete("/profile/:id", UsersCtrl.deleteUser);
+  app.get("/avatar/:id", UsersCtrl.getAvatar);
+  app.get("/rooms", ChatCtrl.listRooms);
+  app.post("/rooms", ChatCtrl.makeRoom);
+  app.get("/room/:name", ChatCtrl.getRoom);
+  app.delete("/room/:name", ChatCtrl.deleteRoom);
 };
