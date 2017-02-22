719aef363113695274dd189635d959afe4352f0f
keyserbrian1 <keyserbrian1@github.com>
2017-02-22 08:28:23 -0600

    pre-merge cleanup

--- client/static/scripts/factories/UserFactory.js
+++ client/static/scripts/factories/UserFactory.js
@@ -1,38 +1,26 @@
 
-app.factory('UserFactory', ['$http', '$routeParams', function ($http, $routeParams) {
+app.factory("UserFactory", ["$http", "$routeParams", function ($http, $routeParams) {
 
-    var factory = {};
+  var factory = {};
 
     // Retrieve a list of all users
-    factory.index = function(callback) {
-        return $http.get('/profiles')
-    };
+  factory.index = function() {
+    return $http.get("/profiles");
+  };
 
     // Retrieve a subset of users
     // Possibly refactor to get with filter information in url
-    factory.subset = function(filter) {
-        return $http.post('/profiles/subset', filter)
-    };
-
-    // Retrieve data for one user
-    factory.show = function (callback) {
-        return $http.get(`/profiles/${$routeParams.id}`)
+  factory.subset = function(filter) {
+    throw "Not Implemented";
+    // return $http.post('/profiles/subset', filter)
+  };
+  factory.get = function(id) {
+    if (id){
+      return $http.get("/profile/"+id);
+    } else {
+      return $http.get("/profile");
     }
+  };
 
-    // Create a new user
-    factory.create = function(newUser) {
-        return $http.post('/profiles', newUser)
-    };
-
-    // Update user information
-    factory.update = function(editedUser) {
-        $http.patch(`/profiles/${$routeParams.id}`, editedUser)
-    }
-
-    // Delete a user
-    factory.delete = function(callback) {
-        return $http.delete(`/profiles/${$routeParams.id}`)
-    }
-
-    return factory;
+  return factory;
 }]);
