b5b0874f47bf0c5c3cb8399b8ac43c8329822168
= <salas.luis40@gmail.com>
2017-02-22 14:06:17 -0600

    begin avatar change

--- client/static/scripts/app.js
+++ client/static/scripts/app.js
@@ -3,7 +3,7 @@ var app = angular.module("app", ["ngRoute"]);
 app.config(function($routeProvider){
     $routeProvider
         .when('/',{
-            templateUrl:"partials/register.html"
+            templateUrl:"partials/login.html"
         })
         .when('/login',{
             templateUrl:"partials/login.html"
