6f2668c8a9adddd387c668a15ff84727caf1d088
keyserbrian1 <keyserbrian1@github.com>
2017-02-22 08:28:52 -0600

    pre-merge cleanup

--- client/static/scripts/controllers/UserController.js
+++ client/static/scripts/controllers/UserController.js
@@ -1,34 +1,33 @@
 app.controller("UserController", ["$scope", "UserFactory", function ($scope, UserFactory) {
-    $scope.users = [];
-    $scope.user = {};
+  $scope.users = [];
+  $scope.user = {};
 
-    $scope.setUsers = function() {
-        UserFactory.index(function (data) {
-            $scope.users = data;
-        });
-    }
-    $scope.setUsers();
+  $scope.setUsers = function() {
+    UserFactory.index(function (data) {
+      $scope.users = data;
+    });
+  };
+  $scope.setUsers();
 
-    function setUser(data) {
-        $scope.user = data;
-        $scope.newUser = {};
-    }
+  function setUser(data) {
+    $scope.user = data;
+    $scope.newUser = {};
+  }
 
-    $scope.show = function() {
-        UserFactory.show(setUser);
-    }
+  $scope.show = function() {
+    UserFactory.show(setUser);
+  };
 
-    $scope.create = function (newUser)
-    {
-        UserFactory.create(newUser, setUsers);
-    };
+  $scope.create = function (newUser)    {
+    UserFactory.create(newUser, setUsers);
+  };
 
-    $scope.update = function() {
-        UserFactory.update($scope.newUser, setUsers);
-        newUser = {};
-    }
+  $scope.update = function() {
+    UserFactory.update($scope.newUser, setUsers);
+    newUser = {};
+  };
 
-    $scope.delete = function(id) {
-        UserFactory.delete(setUsers);
-    }
+  $scope.delete = function(id) {
+    UserFactory.delete(setUsers);
+  };
 }]);
