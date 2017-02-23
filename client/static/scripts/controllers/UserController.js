230d03590da8712ca7f90440060a2bc22ee1f70b
keyserbrian1 <keyserbrian1@github.com>
2017-02-23 11:51:52 -0600

    Avatar upload and download is working

--- client/static/scripts/controllers/UserController.js
+++ client/static/scripts/controllers/UserController.js
@@ -1,11 +1,11 @@
 app.controller("UserController", ["$scope", "Upload", "$location", "UserFactory", function ($scope, Upload, $location, UserFactory) {
   $scope.login = function(){
     UserFactory.login($scope.user).then(res=>{
-      console.log(res)
+      console.log(res);
       if(!res.data.err){
         $location.url("/rooms");
       } else {
-        $scope.validations = res.data.err.items[0]
+        $scope.validations = res.data.err.items[0];
         throw res.data.err;
       }
       return null;
@@ -26,45 +26,47 @@ app.controller("UserController", ["$scope", "Upload", "$location", "UserFactory"
   };
   $scope.logout = function(){
     UserFactory.logout().then(function(res){
-      console.log(res)
-      $location.url('/login')
-    })
-  }
+      console.log(res);
+      $location.url("/login");
+      return null;
+    }).catch(console.error);
+  };
   $scope.index = function(){
     UserFactory.index().then(userData=>{
       $scope.user = userData.data;
-      console.log(userData.data)
-      // return null;
+      console.log(userData.data);
+      return null;
     }).catch(console.error);
   };
   $scope.update = function(){
-    console.log($scope.user)
+    console.log($scope.user);
     UserFactory.update($scope.user).then(data=>{
-      console.log(data)
-    })
-  }
+      console.log(data);
+      return null;
+    }).catch(console.error);
+  };
   $scope.upload = function (file){
     Upload.upload({
-            url: '/profile',
-            data: {avatar: file}
-        }).then(function (res) {
-            console.log('here')
-            console.log(res)
-            console.log('Success ' + res.config.data.file.name + 'uploaded. Response: ' + res.data);
-        }, function (res) {
-            console.log('Error status: ' + res.status);
-        });
-    console.log(file)
-    };
+      url: "/profile",
+      data: {avatar: file}
+    }).then(function (res) {
+      console.log("here");
+      console.log(res);
+      console.log("Success " + res.config.data.filename + "uploaded. Response: ",res.data);
+      return null;
+    }).catch(function (res) {
+      console.log("Error status: " + res.status);
+    });
+    console.log(file);
+  };
 
 
   $scope.submit = function() {
-      if ($scope.form.file.$valid && $scope.file) {
-        console.log($scope.file)
-        $scope.upload($scope.file);
-    }
-    else{
-      console.log('file is not valid')
+    if ($scope.form.file.$valid && $scope.file) {
+      console.log($scope.file);
+      $scope.upload($scope.file);
+    }    else{
+      console.log("file is not valid");
     }
   };
 
