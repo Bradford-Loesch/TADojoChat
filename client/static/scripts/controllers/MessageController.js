f1818eebb68475c32045917b95838c20de7b2329
Bradford Loesch <bradford.loesch@gmail.com>
2017-03-02 11:11:09 -0600

    socket bugs

--- client/static/scripts/controllers/MessageController.js
+++ client/static/scripts/controllers/MessageController.js
@@ -28,7 +28,7 @@ app.controller("MessageController", ["$scope", "$routeParams", "$location", "Soc
   }
   getUser();
   getMessages();
-  joinRoom();
+  setTimeout(joinRoom,500);
 
   // post new message
   $scope.sendMessage = function() {
@@ -61,11 +61,10 @@ app.controller("MessageController", ["$scope", "$routeParams", "$location", "Soc
 
   // reveive data from user connect broadcasts
   SocketFactory.onUserConnect(function(data) {
-    setCurrentUsers(data);
     if ('newuser' in data) {
       $scope.allUsers.push(data.newuser);
     }
-    $scope.$apply();
+    setCurrentUsers(data);
   })
 
   // receive data from user disconnect broadcasts
